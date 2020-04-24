6fda748c95ecf1ad419955e0d159d40f73ea73af
Wendel de Lima <wendel.delima94@gmail.com>
2020-04-24 00:22:02 -0300

    Filtro de pesquisa

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Home from"./components/Home"
+import Filtro from './components/Filtro'
 
 
 class App extends React.Component {
 
   render () {
   return  <div className="App">
-    Teste
-    <Home />
+    <Filtro />
   </div>
   }
 }
--- src/components/Filtro.js
+++ src/components/Filtro.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import './Filtro.css'
 
 
 
 class Filtro extends React.Component {
+ 
+
 
   render () {
   return  <div>
       <div>
-        <img src="" alt=""/>
-        <p>nome do produto</p>
-        <p>preço do produto</p>
-        <button>adicionar ao carrinho</button>
+      
+   <form className="Filter">
+        <h2>Filtros:</h2>
+        <p>Valor Mínimo: </p>
+          <input className="input" type="number" placeholder="" onChange="" />
+        <p>Valor Máximo: </p>
+          <input className="input" type="number" placeholder="" onChange="" />
+        <p>Buscar Produto</p>
+          <input className="input" type="text" placeholder="" />
+   </form>
       </div>
     </div>
   }
