f64a10758dc5705047cae082d7d1a7766ca2c33e
Ricardo Martins de Souza Timbó <rcardms@gmail.com>
2020-04-23 22:50:15 -0300

    lista de produtos no App.js, passado por props para o cardproduto.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import Home from"./components/Home"
+import Filtro from"./components/Filtro"
+import Carrinho from "./components/Carrinho" 
 
+const listaDeProdutos = [{
+  id: 1,
+  imagem: "https://picsum.photos/300/200/?a=1",
+  nomeDoProduto: "produto 1",
+  precoDoProduto: 100.00
+},
+{
+  id: 2,
+  imagem: "https://picsum.photos/300/200/?a=2",
+  nomeDoProduto: "produto 2",
+  precoDoProduto: 120.00
+},
+{
+  id: 3,
+  imagem: "https://picsum.photos/300/200/?a=3",
+  nomeDoProduto: "produto 3",
+  precoDoProduto: 130.00
+},
+{
+  id: 4,
+  imagem: "https://picsum.photos/300/200/?a=4",
+  nomeDoProduto: "produto 4",
+  precoDoProduto: 140.00
+}
+]
 
 class App extends React.Component {
 
+
   render () {
   return  <div className="App">
-    Teste
-    <Home />
+    <h1>Labenu E-commerce</h1>
+    <Home listaDeProdutos={listaDeProdutos} />
   </div>
   }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import CardProdutos  from './CardProdutos';
 
 
 
 class Home extends React.Component {
-
+constructor(props){
+  super(props)
+}
   render () {
-  return  <div>
-      <p>galeria de produtos</p>
+    const listaDeComponentes = this.props.listaDeProdutos.map((produto)=> {
+      return <CardProdutos key={produto.id} infoProduto={produto}/>
+    })
+    return  (
+      <div>
+        
+        <div>
+          <p>Quantidade de Produtos</p> 
+          <select>
+            <option>Preço: Crescente</option>
+            <option>Preço: Decrescente</option>
+          </select>
+        </div>
+        <div>
+          {listaDeComponentes}
+        </div>
       </div>
-  }
+    )
+
 }
 
+}
 export default Home;
