a82c48c4c5ec32a1d152be007dd39be9dd0ce5c0
Ricardo Martins de Souza Timbó <rcardms@gmail.com>
2020-04-26 11:13:00 -0300

    atualizado filtro

--- src/components/Filtro.js
+++ src/components/Filtro.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import './Filtro.css'
 
 
 
 class Filtro extends React.Component {
+ 
+
 
   render () {
-  return  <div>
-
+  return  <div>      
+   <form className="Filter">
+        <h2>Filtros:</h2>
+        <p>Valor Mínimo: </p>
+          <input className="input" type="number" placeholder="" onChange="" />
+        <p>Valor Máximo: </p>
+          <input className="input" type="number" placeholder="" onChange="" />
+        <p>Buscar Produto</p>
+          <input className="input" type="text" placeholder="" />
+   </form>
     </div>
   }
 }
